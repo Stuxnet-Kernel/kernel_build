0afcc18b1d1d51997a854e0e6dcda66bd72cbdae
Jean Francis Dominique S. Mabalot (Gnome@xda) <emceethemouth@users.noreply.github.com>
2014-12-26 03:32:38 +0800

    update script
    Signed-off-by: Jean Francis Dominique S. Mabalot <jmabalot@google.com>

--- shamu-stable.sh
+++ shamu-stable.sh
@@ -22,7 +22,7 @@ export CROSS_COMPILE=~/tmp/arm-eabi-4.9/bin/arm-eabi-
 export ENABLE_GRAPHITE=true
 version="1.0"
 export CONFIG_DEBUG_SECTION_MISMATCH=y
-make mako_defconfig
+make shamu_defconfig
 
 if [ $# -gt 0 ]; then
 echo $1 > .version
@@ -30,42 +30,7 @@ fi
 
 time make -j8 2>&1 | tee kernel.log
 
-echo ""
-echo "Building boot.img"
-cp arch/arm/boot/zImage ../ramdisk-shamu/
-
-cd ../ramdisk-shamu/
-
-echo ""
-echo "building ramdisk"
-./mkbootfs ramdisk | gzip > ramdisk.gz
-echo ""
-echo "making boot image"
-./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02000000 --ramdisk ramdisk.gz --output ../mako/boot.img
-
-rm -rf ramdisk.gz
-rm -rf zImage-dtb
-
-cd ../shamu
-
-zipfile="StuxnetN6-v$version.zip"
-echo ""
-echo "zipping kernel"
-cp boot.img zip/
-
-rm -rf ../ramdisk-shamu/boot.img
-
-cd zip/
-rm -f *.zip
-zip -r -9 $zipfile *
-rm -f /tmp/*.zip
-cp *.zip /tmp
-
-cd ../
-rm arch/arm/boot/zImage-dtb
-rm boot.img
 rm kernel.log
-rm zip/boot.img
 
 echo ""
 echo "****************************************************"
